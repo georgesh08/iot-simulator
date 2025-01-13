6c262be522edf7a633507e35167fb629c8ae66e7
George Shulyak <georgiyshulyak@gmail.com>
2025-01-13 19:02:21 +0300

    add logging, start implementing data simulator

---
+++ DataSimulator/DataSimulator/DataSimulator/DataSimulator.cs
@@ -0,0 +1,11 @@
+﻿namespace DataSimulator;
+
+public class DataSimulator
+{
+	public static void LaunchSimulator(int numberOfDevices, int dataSendPeriod)
+	{
+		
+	}
+	
+	
+}
--- DataSimulator/DataSimulator/DataSimulator/Program.cs
+++ DataSimulator/DataSimulator/DataSimulator/Program.cs
@@ -1,13 +1,24 @@
-﻿namespace DataSimulator;
+﻿using Serilog;
+
+namespace DataSimulator;
 
 internal class Program
 {
 	static void Main(string[] args)
 	{
+		Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+			.WriteTo.Console()
+			.CreateLogger();
+		
 		if (args.Length < 2)
 		{
-			Console.WriteLine("Invalid number of arguments. Should be two: <number of devices> <data send period>");
+			Log.Error("Invalid number of arguments. Should be two: <number of devices> <data send period>");
 			return;
 		}
+		
+		var numberOfDevices = Convert.ToInt32(args[0]);
+		var dataSendPeriod = Convert.ToInt32(args[1]); // in seconds
+		
+		DataSimulator.LaunchSimulator(numberOfDevices, dataSendPeriod);
 	}
 }
