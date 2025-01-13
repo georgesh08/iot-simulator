7789e40c4f769576ad8f63c844edabed3cd449b3
George Shulyak <georgiyshulyak@gmail.com>
2025-01-13 16:28:47 +0300

    add sensor device type

---
+++ DataSimulator/DataSimulator/DataSimulator/Device/SensorDevice.cs
@@ -0,0 +1,26 @@
+﻿using DataSimulator.Base;
+
+namespace DataSimulator.Device;
+
+public class SensorDevice : ABaseIoTDevice<SensorDeviceValue>
+{
+	public SensorDevice(string name) : base(name)
+	{
+		valueProducerScheduler.Interval = TimeSpan.FromSeconds(2);
+	}
+
+	public override IoTDeviceType DeviceType => IoTDeviceType.SENSOR;
+	
+	protected override void ProduceValue()
+	{
+		var bytesToGenerate = Random.Shared.Next(2, 11);
+		var newBytes = new byte[bytesToGenerate];
+		Random.Shared.NextBytes(newBytes);
+
+		LastProducedValue = new SensorDeviceValue
+		{
+			Data = newBytes,
+			Timestamp = DateTime.Now
+		};
+	}
+}
---
+++ DataSimulator/DataSimulator/DataSimulator/Device/SensorDeviceValue.cs
@@ -0,0 +1,7 @@
+﻿namespace DataSimulator.Device;
+
+public struct SensorDeviceValue
+{
+	public byte[] Data { get; set; }
+	public DateTime Timestamp { get; set; }
+}
