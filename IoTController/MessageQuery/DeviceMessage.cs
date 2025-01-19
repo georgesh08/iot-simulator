4c52c326ac1f191811b41731d2c7ed9b6a3e86f4
George Shulyak <georgiyshulyak@gmail.com>
2025-01-19 19:00:47 +0300

    implement basic message broker

---
+++ IoTController/MessageQuery/DeviceMessage.cs
@@ -0,0 +1,10 @@
+﻿using IoTServer;
+
+namespace MessageQuery;
+
+public class DeviceMessage
+{
+	public string DeviceId { get; set; }
+	public DeviceProducedValue Value { get; set; }
+	public ulong Timestamp { get; set; }
+}
---
+++ IoTController/MessageQuery/RabbitMQ/RabbitMQPublisher.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace MessageQuery.RabbitMQ;
+
+public class RabbitMQPublisher : IMessagePublisher, IDisposable
+{
+	private IConnection connection;
+	private IModel channel;
+	private const string ExchangeName = "iot.device.data";
+	private const string QueueName = "device_data";
+	private const string RoutingKey = "device.data";
+	private readonly string hostname;
+
+	public RabbitMQPublisher()
+	{
+		hostname = Environment.GetEnvironmentVariable("RABBITMQ_HOSTNAME") ?? "localhost";
+		
+		var factory = new ConnectionFactory
+		{
+			HostName = hostname
+		};
+		
+		connection = factory.CreateConnection();
+		channel = connection.CreateModel();
+		
+		channel.ExchangeDeclare(ExchangeName, ExchangeType.Direct, durable: true);
+		channel.QueueDeclare(QueueName, durable: true, exclusive: false, autoDelete: false);
+		channel.QueueBind(QueueName, ExchangeName, RoutingKey);
+	}
+	
+	public async Task PublishDeviceDataAsync(DeviceMessage message)
+	{
+		var messageBody = JsonConvert.SerializeObject(message);
+		var body = Encoding.UTF8.GetBytes(messageBody);
+
+		var properties = CreateProps();
+		
+		channel.BasicPublish(ExchangeName, RoutingKey, properties, body);
+	}
+	
+	public void Dispose()
+	{
+		channel.Dispose();
+		connection.Dispose();
+	}
+
+	private IBasicProperties CreateProps()
+	{
+		var properties = channel.CreateBasicProperties();
+		properties.Persistent = true; // Save messages on disk
+		properties.MessageId = Guid.NewGuid().ToString();
+		properties.Timestamp = new AmqpTimestamp(DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+		
+		return properties;
+	}
+}
