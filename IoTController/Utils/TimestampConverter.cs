db6235b8cb350fa1d304cebe4906932fdfc6ddb6
George Shulyak <georgiyshulyak@gmail.com>
2025-01-20 11:21:03 +0300

    addd utils

---
+++ IoTController/Utils/TimestampConverter.cs
@@ -0,0 +1,18 @@
+﻿namespace Utils;
+
+public class TimestampConverter
+{
+	public static ulong ConvertToTimestamp(DateTime dateTime)
+	{
+		var utcDateTime = dateTime.ToUniversalTime();
+		
+		var timestamp = (ulong)utcDateTime.Subtract(DateTime.UnixEpoch).TotalMilliseconds;
+
+		return timestamp;
+	}
+
+	public static DateTime ConvertFromTimestamp(ulong timestamp)
+	{
+		return DateTime.UnixEpoch.AddMilliseconds(timestamp);
+	}
+}
