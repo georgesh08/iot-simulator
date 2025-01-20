e1593d04077fa08dda4160c3743dc2c65100bb1a
George Shulyak <georgiyshulyak@gmail.com>
2025-01-20 13:55:09 +0300

    rule engine response dto

---
+++ IoTController/MessageQuery/RuleEngineResult.cs
@@ -0,0 +1,14 @@
+﻿namespace MessageQuery;
+
+public class RuleEngineResult
+{
+	public string Message { get; set; }
+	public string DeviceId { get; set; }
+	public Status EngineVerdict { get; set; }
+}
+
+public enum Status
+{
+	Ok = 0,
+	Error = 1
+}
