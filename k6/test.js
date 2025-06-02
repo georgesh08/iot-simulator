72c2df517781eec0068c8b7962cd457c5aaf67ca
Maxim Admakin <maxim.admakin@gmail.com>
2025-06-02 17:22:31 +0300

    k6 fix local libs

--- k6/test.js
+++ k6/test.js
@@ -55,8 +55,8 @@ function generateDeviceData(deviceId) {
   };
 }
 
-import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
-import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+import { htmlReport } from "./k6-libs/bundle.js";
+import { textSummary } from "./k6-libs/index.js";
 
 export function handleSummary(data) {
   return {
